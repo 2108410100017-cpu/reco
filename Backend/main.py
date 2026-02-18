25cd4418ba26a3cbe5cae9581ee4bf5d87ff9528
Anurag <2108410100017@recsonbhadra.ac.in>
2026-02-18 15:30:50 +0530

    add torch

--- Backend/main.py
+++ Backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="Image Recommendation API")
 # -------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://reco-2-diph.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
